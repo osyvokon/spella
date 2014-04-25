651c45c8b947380743fb47c1cc04ab1eb43b8cba
Aleksey Sivokon <aleksey.sivokon@gmail.com>
2014-04-25 21:02:41 +0300

    Bring text in <p> to uppercase.
    Not super useful, just preparing to do real API call.

--- chrome-extension/js/checker.js
+++ chrome-extension/js/checker.js
@@ -1,12 +1,21 @@
 var PageContentSpellChecker = function () {
 
-  function requestCorrections(textChunks) {
-  }
+  var API = {
+    requestCorrections: function(textChunks) {
+      return $.map(textChunks, function(p) { return p.toUpperCase(); });
+    }
+  };
+
 
   return {
     validatePage: function () {
-      return $.map($("p"), function(n) {
+      var pp = $("p");
+      var chunks = $.map(pp, function(n) {
         return n.textContent;
+      });
+      var validated = API.requestCorrections(chunks);
+      _.each(_.zip(pp, validated), function(t) {
+        $(t[0]).html(t[1]);
       });
     }
   };
