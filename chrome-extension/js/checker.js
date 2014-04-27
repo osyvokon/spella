20c64f1da1ad551189f95420bd92d7f1cef1de47
Aleksey Sivokon <aleksey.sivokon@gmail.com>
2014-04-27 17:26:04 +0300

    Check page content and highligh mistakes.
    Replacing source elements is now quite working yet.

--- chrome-extension/js/checker.js
+++ chrome-extension/js/checker.js
@@ -1,25 +1,71 @@
 var PageContentSpellChecker = function () {
 
   var API = {
-    requestCorrections: function(textChunks) {
-      return $.map(textChunks, function(p) { return p.toUpperCase(); });
+
+    baseUrl: "http://localhost:8080",
+
+    requestCorrections: function(textChunks, callback) {
+      $.ajax(this.baseUrl + "/api/check", {
+        data : JSON.stringify({"text": textChunks}),
+        contentType : 'application/json',
+        type : 'POST',
+        success: function (response) {
+          callback(response.checked);
+        }
+      });
+
+      //return $.map(textChunks, function(p) { return p.toUpperCase(); });
     }
   };
 
 
   return {
     validatePage: function () {
-      var pp = $("p");
-      var chunks = $.map(pp, function(n) {
-        return n.textContent;
+      var pp = _.filter($("p,h1,h2,h3,h4"), function (p) {
+        return p && p.textContent && (p.textContent.trim() != "");
       });
-      var validated = API.requestCorrections(chunks);
-      _.each(_.zip(pp, validated), function(t) {
-        $(t[0]).html(t[1]);
+      var chunks = $.map(pp, function(n) { return n.textContent; });
+      API.requestCorrections(chunks, function (validated) {
+        console.log("Got response")
+        for (var chunkIndex = 0; chunkIndex < validated.length; chunkIndex++) {
+          var p = pp[chunkIndex];
+          var v = $($.parseXML(validated[chunkIndex]));
+          var startIndex = {}, endIndex = {};
+          var stopPoints = [];
+          _.each(v.find("error"), function (error) {
+            var from = Number(error.attributes['fromx'].value),
+                to   = Number(error.attributes['tox'].value);
+            stopPoints.push(from);
+            stopPoints.push(to);
+            startIndex[from] = error;
+            endIndex[to] = error;
+          });
+
+          if (stopPoints.length > 0) {
+            console.log("Found error in chunk: ", p.textContent);
+          } else {
+            continue;
+          }
+
+          var out = [];
+          var i = 0;
+          var s = p.textContent;
+          _.each(stopPoints, function (j) {
+            out.push(s.slice(i, j));
+            if (startIndex[j]) out.push("<span class='spellingError' style='background-color: yellow'>");
+            if (endIndex[j]) out.push("</span>");
+            i = j;
+          });
+          out.push(s.slice(i, s.length));
+          console.log(out.join(''));
+
+          $(p).html(out.join(''));
+          //$(p).html(p.textContent.toUpperCase());
+        }
       });
     }
-  };
+  }
 }
 
 var result = PageContentSpellChecker().validatePage();
-console.log(result);
+console.log("Plugin started");
