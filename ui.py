5f0f0956dc124fa9d968f9351c77daff05f5da4d
Aleksey Sivokon <aleksey.sivokon@gmail.com>
2014-06-11 07:37:49 -0400

    Helper message on GET /api/check method

--- ui.py
+++ ui.py
@@ -20,8 +20,12 @@ def proxy():
         "text": text})
     return r.text
 
+@app.route("/api/check", methods=["GET"])
+def check_get():
+    return "Only POST method supported"
+
 @app.route("/api/check", methods=["POST", "OPTIONS"])
-@crossdomain(origin="*", headers="Content-Type")
+@crossdomain(origin="*", headers="*")
 def check():
     texts = request.json['text']
     if not isinstance(texts, list):
